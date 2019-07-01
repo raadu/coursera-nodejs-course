635e3a9722f7a4cfeed1f362faf089cabf4c3b34
raadu <raiyad.raad@gmail.com>
2019-07-01 20:35:58 +0600

    node-http lesson done

--- NodeJS/node-http/index.js
+++ NodeJS/node-http/index.js
@@ -1,14 +1,64 @@
-const http = require('http');
+const http = require("http");
 const hostname = "localhost";
 const port = 3000;
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req,res) => 
 {
-	console.log(req.headers);
+	console.log("Request for "+req.url+" by method "+req.method);
 
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/html');
-	res.end('<html><body></body><h1>Hello World</h1></body></html>');
+	if(req.method == 'GET')
+	{
+		var fileUrl;
+		if(req.url == "/")
+		{
+			fileUrl = "/index.html"
+		}
+		else 
+		{
+			fileUrl = req.url;
+		}
+
+		var filePath = path.resolve("./public"+fileUrl);
+		const fileExt = path.extname(filePath);
+		if(fileExt == ".html")
+		{
+			fs.exists(filePath, (exists) => 
+			{
+				if(!exists)
+				{
+					res.statusCode = 404;
+					res.setHeader("Content-Type","text/html");
+					res.end("<html><body><h1>not found</h1></body></html>");
+
+					return;
+				}
+				else
+				{
+					res.statusCode = 200;
+					res.setHeader("Content-Type","text/html");
+					fs.createReadStream(filePath).pipe(res);
+				}
+			})
+		}
+		else
+		{
+			res.statusCode = 404;
+			res.setHeader("Content-Type","text/html");
+			res.end("<html><body><h1>not an HTML file</h1></body></html>");
+
+			return;
+		}
+	}
+	else 
+	{
+		res.statusCode = 404;
+		res.setHeader("Content-Type","text/html");
+		res.end("<html><body><h1>not supported request</h1></body></html>");
+
+		return;
+	}
 })
 
 server.listen(port, hostname, () => 
