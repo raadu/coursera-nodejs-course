4a1e852ab4ed5e25e8f09175e9fa62506d133fe3
raadu <raiyad.raad@gmail.com>
2019-07-05 01:52:07 +0600

    morgan node module added

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const http = require("http");
 const hostname = "localhost";
 const port = 3000;
+const morgan = require("morgan");
 
 const app = express();
 
+app.use(morgan("dev"));
+app.use(express.static(__dirname+ "/public"));
+
 app.use((req, res, next) => 
 {
-	console.log(req.headers);
 	res.statusCode = 200;
 	res.setHeader("Content-Type", "text/html");
 	res.end("<htm><body><h1>This is an express server</h1></body></html>");
