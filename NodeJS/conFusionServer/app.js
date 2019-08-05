ef9d839d5b9a48e35104ae445fe031c7d6902b51
raadu <raiyad.raad@gmail.com>
2019-08-05 20:10:39 +0600

    Express Sessions Part-1 Added

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require("mongoose");
+var session = require('express-session');
+var FileStore = require('session-file-store')(session)
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,7 +16,7 @@ var leaderRouter = require("./routes/leaderRouter");
 const Dishes = require("./models/dishes");
 
 const url = "mongodb://localhost:27017/conFusion";
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url, { useNewUrlParser: true });
 
 connect.then((db) =>
 {
@@ -33,14 +35,21 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("123-456-789"));
+//app.use(cookieParser("123-456-789"));
+app.use(session({
+	name: "session-id",
+	secret: "12345-67890-09876-54321",
+	saveUninitialized: false,
+	resave: false,
+	store: new FileStore()
+}));
 
 //User authorization middleware
 function auth(req,res,next)
 {
-	console.log(req.signedCookies);
+	console.log(req.session);
 
-	if(!req.signedCookies.user)
+	if(!req.session.user)
 	{
 
 	var authHeader = req.headers.authorization;
@@ -60,7 +69,7 @@ function auth(req,res,next)
 
 	if(username === "admin" && password === "password")
 	{
-		res.cookie("user","admin",{signed:true})
+		req.session.user = "admin";
 		next();
 	}
 	else
@@ -74,7 +83,7 @@ function auth(req,res,next)
 
 	else
 	{
-		if(req.signedCookies.user === "admin")
+		if(req.session.user === "admin")
 		{
 			next();
 		}
